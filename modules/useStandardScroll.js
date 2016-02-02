7ff3d2fa0996251c25a31e0185b6cbcc227222da
Clark Bynum <cbynum@clearblade.com>
2016-02-02 14:38:20 -0600

    added timeout to update scroll

--- modules/useStandardScroll.js
+++ modules/useStandardScroll.js
@@ -31,7 +31,10 @@ export default function useStandardScroll(createHistory) {
     currentKey = key
 
     const scrollPosition = getScrollPosition() || [ 0, 0 ]
-    window.scrollTo(...scrollPosition)
+    setTimeout(function() {
+      window.scrollTo(...scrollPosition)
+    })
+
   }
 
   let unsetScrollRestoration, unlistenScroll, unlistenBefore
